e367c173349e871f9fbe83ea597b65f8fe8db626
aasjunior <adeldivo@hotmail.com>
2024-08-28 00:02:42 -0300

    docker-compose

--- sofia-api/src/main/java/com/sofia/backend/domain/service/auth/TokenService.java
+++ sofia-api/src/main/java/com/sofia/backend/domain/service/auth/TokenService.java
@@ -48,7 +48,7 @@ public class TokenService {
     private Instant generateExpirationDate(){
         return LocalDateTime
                 .now()
-                .plusHours(2)
+                .plusHours(20)
                 .toInstant(ZoneOffset.of("-03:00"));
     }
 
